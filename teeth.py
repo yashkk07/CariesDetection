7be14749fb0dfc1b94b6cc367b0ad39373aa065c
yashkk07 <ykarnani@gitam.in>
2024-12-14 21:45:17 +0530

    add files

---
+++ teeth.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+dataset_path="/teeth_dataset"
+training_path="/teeth_dataset/Trianing"
+testing_path="/teeth_dataset/test"
+
